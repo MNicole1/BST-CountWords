1e5fe8351dde65b994cb89cc27df67f33aa4995d
MNicole1 <mnicole1@msudenver.edu>
2017-01-31 09:30:09 -0700

    Initial Files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-BST-MostCommonString
+Find the most common word in a space delineated word file using a binary search tree for indexing.
---
+++ cmake-build-debug/CMakeFiles/BST_MostCommonString.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BST_MostCommonString.dir/main.c.o"
+  "BST_MostCommonString.pdb"
+  "BST_MostCommonString"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BST_MostCommonString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
