4562c6ed67f9de476365a648585654643892e439
MNicole1 <mnicole1@msudenver.edu>
2017-02-09 23:36:37 -0700

    Preparing to move subtree functions.

---
+++ BSTNode.c
@@ -0,0 +1,5 @@
+//
+// Created by Marcello Nicoletti on 2/9/17.
+//
+
+#include "BSTNode.h"
---
+++ BSTNode.h
@@ -0,0 +1,8 @@
+//
+// Created by Marcello Nicoletti on 2/9/17.
+//
+
+#ifndef BST_MOSTCOMMONWORD_BSTNODE_H
+#define BST_MOSTCOMMONWORD_BSTNODE_H
+
+#endif //BST_MOSTCOMMONWORD_BSTNODE_H
